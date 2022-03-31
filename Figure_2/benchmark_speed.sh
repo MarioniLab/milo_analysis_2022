1c256eef1e7538b10ce5946413833282f2bc12c3
akluzer <alice.kluzer@cruk.cam.ac.uk>
2022-03-31 14:57:03 +0100

    Figure 2

---
+++ Figure_2/benchmark_speed.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## Activate conda env
+## source activate base
+source /hps/software/users/marioni/akluzer/miniconda3/etc/profile.d/conda.sh
+conda activate milo_bm
+
+## Run embryo
+echo "Rscript /nfs/research/marioni/akluzer/milo_analysis_2022/Figure_2/benchmark_speed.R" | bsub -q production -o /nfs/research/marioni/akluzer/milo_analysis_2022/Figure_2/speed_bm.out -e /nfs/research/marioni/akluzer/milo_analysis_2022/Figure_2/speed_bm.err -R"select[mem>50000] rusage[mem=50000]" -M50000
