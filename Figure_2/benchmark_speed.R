9d4f02899c68e748c95a299067bbb0bb89cb2e3d
akluzer <alice.kluzer@cruk.cam.ac.uk>
2022-04-06 10:31:42 +0100

    updated figure2

--- Figure_2/benchmark_speed.R
+++ Figure_2/benchmark_speed.R
@@ -607,43 +607,43 @@ time.df <- cbind(n_cells, reduced_dim, graph, mem_red, mem_graph)
 time.df <- data.frame(time.df) 
 write_csv(time.df, "/nfs/research/marioni/akluzer/time_df.csv")
 
-# same as above, but for different values of k
-reduced_dimk <- rep(NA, 10)
-graphk <- rep(NA, 10)
-k_val <- rep(NA, 10)
-j <- 1
-
-# down sample the milo object
-traj_milo <- sim_milo[,sample(sim_milo@colData@rownames, 100000)]
-
-for (k in seq(10, 70, 20)) { 
-    
-    print(k)
-    
-    # create design matrix to explain experimental design 
-    traj_design <- data.frame(colData(traj_milo))[,c("Sample", "Condition")]
-    traj_design <- distinct(traj_design)
-    rownames(traj_design) <- traj_design$Sample
-    print(traj_design) 
-    
-    ### using system.time() calculate elapsed time
-    c <- system.time({traj_milo2 <- miloR::buildGraph(traj_milo, k = k, d = 30);
-    traj_milo2 <- makeNhoods2(traj_milo, prop = 0.1, k = k, d=30, refined = TRUE, refinement_scheme = "reduced_dim");
-    traj_milo2 <- miloR::countCells(traj_milo2, meta.data = data.frame(colData(traj_milo)), sample="Sample");
-    traj_milo2 <- miloR::calcNhoodDistance(traj_milo2, d=30);
-    traj_milo2 <- testNhoods2(traj_milo2, design = ~ Condition, design.df = traj_design, fdr.weighting = "k-distance")})
-
-    d <- system.time({traj_milo1 <- miloR::buildGraph(traj_milo, k = k, d = 30);
-    traj_milo1 <- makeNhoods2(traj_milo1, prop = 0.1, k = k, d=30, refined = TRUE, refinement_scheme = "graph");
-    traj_milo1 <- miloR::countCells(traj_milo1, meta.data = data.frame(colData(traj_milo)), sample="Sample");
-    traj_milo1 <- testNhoods2(traj_milo1, design = ~ Condition, design.df = traj_design, fdr.weighting = "graph-overlap")})
-
-    k_val[j] <- k
-    reduced_dimk[j] <- c[3]
-    graphk[j] <- d[3]
-    j <- j + 1
-}
-
-k.df <- cbind(k_val, reduced_dimk, graphk)
-k.df <- data.frame(k.df) 
-write_csv(k.df, "/nfs/research/marioni/akluzer/k_df.csv")
+# # same as above, but for different values of k
+# reduced_dimk <- rep(NA, 10)
+# graphk <- rep(NA, 10)
+# k_val <- rep(NA, 10)
+# j <- 1
+# 
+# # down sample the milo object
+# traj_milo <- sim_milo[,sample(sim_milo@colData@rownames, 100000)]
+# 
+# for (k in seq(10, 70, 20)) { 
+#     
+#     print(k)
+#     
+#     # create design matrix to explain experimental design 
+#     traj_design <- data.frame(colData(traj_milo))[,c("Sample", "Condition")]
+#     traj_design <- distinct(traj_design)
+#     rownames(traj_design) <- traj_design$Sample
+#     print(traj_design) 
+#     
+#     ### using system.time() calculate elapsed time
+#     c <- system.time({traj_milo2 <- miloR::buildGraph(traj_milo, k = k, d = 30);
+#     traj_milo2 <- makeNhoods2(traj_milo, prop = 0.1, k = k, d=30, refined = TRUE, refinement_scheme = "reduced_dim");
+#     traj_milo2 <- miloR::countCells(traj_milo2, meta.data = data.frame(colData(traj_milo)), sample="Sample");
+#     traj_milo2 <- miloR::calcNhoodDistance(traj_milo2, d=30);
+#     traj_milo2 <- testNhoods2(traj_milo2, design = ~ Condition, design.df = traj_design, fdr.weighting = "k-distance")})
+# 
+#     d <- system.time({traj_milo1 <- miloR::buildGraph(traj_milo, k = k, d = 30);
+#     traj_milo1 <- makeNhoods2(traj_milo1, prop = 0.1, k = k, d=30, refined = TRUE, refinement_scheme = "graph");
+#     traj_milo1 <- miloR::countCells(traj_milo1, meta.data = data.frame(colData(traj_milo)), sample="Sample");
+#     traj_milo1 <- testNhoods2(traj_milo1, design = ~ Condition, design.df = traj_design, fdr.weighting = "graph-overlap")})
+# 
+#     k_val[j] <- k
+#     reduced_dimk[j] <- c[3]
+#     graphk[j] <- d[3]
+#     j <- j + 1
+# }
+# 
+# k.df <- cbind(k_val, reduced_dimk, graphk)
+# k.df <- data.frame(k.df) 
+# write_csv(k.df, "/nfs/research/marioni/akluzer/k_df.csv")
--- Figure_2/benchmark_speed.sh
+++ Figure_2/benchmark_speed.sh
@@ -6,4 +6,4 @@ source /hps/software/users/marioni/akluzer/miniconda3/etc/profile.d/conda.sh
 conda activate milo_bm
 
 ## Run embryo
-echo "Rscript /nfs/research/marioni/akluzer/milo_analysis_2022/Figure_2/benchmark_speed.R" | bsub -q production -o /nfs/research/marioni/akluzer/milo_analysis_2022/Figure_2/speed_bm.out -e /nfs/research/marioni/akluzer/milo_analysis_2022/Figure_2/speed_bm.err -R"select[mem>50000] rusage[mem=50000]" -M50000
+echo "Rscript /nfs/research/marioni/akluzer/milo_analysis_2022/Figure_2/benchmark_speed.R" | bsub -q production -o /nfs/research/marioni/akluzer/milo_analysis_2022/Figure_2/speed_bm.out -e /nfs/research/marioni/akluzer/milo_analysis_2022/Figure_2/speed_bm.err -R"select[mem>70000] rusage[mem=70000]" -M70000
